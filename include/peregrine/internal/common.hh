f3fbb69b4d40acdd7bfc8305c6d5232f34947f92
Justus Calvin <justus.c79@gmail.com>
2024-04-01 18:14:45 -0700

    Add literals to the peregrine name space.

--- include/peregrine/internal/common.hh
+++ include/peregrine/internal/common.hh
@@ -4,3 +4,13 @@
 #define PEREGRINE_UNLIKELY(x)  __builtin_expect(!!(x), 0)
 
 #define PEREGRINE_FORCE_INLINE __attribute__((always_inline)) inline
+
+#include <string>
+#include <string_view>
+
+namespace peregrine {
+
+using namespace std::string_view_literals;
+using namespace std::string_literals;
+
+} // namespace peregrine
